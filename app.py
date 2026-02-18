5a27cb4d4100b5ab863a8e18cff35ef4fc76b460
L3galSton3r <anthony.mario420@gmail.com>
2026-02-18 23:32:04 +0200

    Refactor app.py for clarity and organization

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ SYMBOL_MAPPINGS = {
     'BTCUSD': 'BTCUSD.m',
     'NAS100': 'US100.std',
     'US30': 'US30.std',
-    'DE40': 'GER40.std',
+    'DE40': 'DE40.std',
     'US500': 'US500.std'
 }
 
@@ -187,4 +187,5 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
 
     app.run(host='0.0.0.0', port=port)
+
 
